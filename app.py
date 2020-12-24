ec62f395531385ee1cf696593ea5cb3dcedce6aa
Kristian Møller Schmidt <34898026+KristianMSchmidt@users.noreply.github.com>
2020-12-24 15:08:30 +0100

    Converted portfolio into flask application

---
+++ app.py
@@ -0,0 +1,29 @@
+# Setup instuctions for Flask in Visual Studio Code
+# 1) Make folder/directory for flask project
+# 2) Go to this folder and open terminal 
+# 3) Write command "python -m venv env" (this makes the virtual environment)
+# 4) Ctrl + Shift + P --> Python select intrepreter --> select the one with "env"   
+# 5) In therminal: "pip install flask"
+# 6) In terminal: "python app.py"
+
+from flask import Flask, render_template, request
+app = Flask('__name__')
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
