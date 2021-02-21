515d4865b903ed4913a4d3601f12a6f1b7572bd0
KristianMSchmidt <krms@kvuc.dk>
2021-02-21 08:57:48 +0100

    changed styling of projects page

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-# Setup instuctions for Flask in Visual Studio Code
-# 1) Open folder with flask project
-# 2) In terminal: "pip install flask" (if necessary)
-# 3) In terminal: "python app.py"
-
 from flask import Flask, render_template, request
 app = Flask('__name__')
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
